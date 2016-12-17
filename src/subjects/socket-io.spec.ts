da063017346e15656a9ea0bf17562eae720f0543
MoshMage <moshmage@gmail.com>
2016-12-17 19:47:41 +0000

    trying to make karma-typescript function

---
+++ src/subjects/socket-io.spec.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by Mosh Mage on 12/17/2016.
+ */
+import {IO} from './socket-io';
+
+describe('IO', () => {
+    it ('is true', () => {
+        expect(true).toBe(true);
+    })
+});
