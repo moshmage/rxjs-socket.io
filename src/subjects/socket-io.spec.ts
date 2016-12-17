041d1f6db9258d8a576915f14ea4e25ba42d7718
MoshMage <moshmage@gmail.com>
2016-12-17 13:31:53 +0000

    rollback on test framework chosen - investigating more

--- src/subjects/socket-io.spec.ts
+++ src/subjects/socket-io.spec.ts
@@ -1,3 +1,13 @@
 /**
  * Created by Mosh Mage on 12/17/2016.
  */
+
+import {IO} from './socket-io';
+
+describe('IO', () => {
+    const _IO = new IO();
+
+    it('created a proper IO class', () => {
+        expect(_IO instanceof IO).toBeTruthy();
+    });
+});
