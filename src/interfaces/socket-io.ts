25a81be807c449ef83bdca21c2a4837d23a2e262
MoshMage <moshmage@gmail.com>
2016-12-22 18:21:33 +0000

    remove initialSocketState because we are using ReplaySubjects

--- src/interfaces/socket-io.ts
+++ src/interfaces/socket-io.ts
@@ -1,3 +1,2 @@
 export interface IoEventInfo { name: string, count?: number; once?: boolean}
 export interface SocketState { connected: boolean, id?: string; }
-export const initialState: SocketState = {connected: false};
